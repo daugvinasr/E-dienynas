c984fda4bf0dacf0fc05e8c65f6c91f44695d75a
Martynas09 <martynas200012@gmail.com>
2021-12-03 18:49:11 +0200

    test

--- resources/views/NamuDarboSarasoLangas.blade.php
+++ resources/views/NamuDarboSarasoLangas.blade.php
@@ -1,10 +1,33 @@
 @extends('layouts.PagrindisLangasSablonas')
-
-
 @section('content')
-    <section class="container mx-auto p-6 rounded-10">
-        <div class="w-full mb-8 overflow-hidden rounded-lg shadow-lg px-2 py-2">
-            <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus ut placeat sint perspiciatis corrupti assumenda nihil explicabo excepturi voluptate voluptas. Iusto, nulla ex atque odit rerum voluptate obcaecati maiores! Dignissimos est, enim aspernatur possimus unde earum corporis ab? Voluptate aliquam est dignissimos commodi doloremque nobis vel at, perspiciatis quasi molestiae?</h1>
-        </div>
-    </section>
+    <div class="flex pt-20 min-h-screen">
+        <div class="col-span-12">
+            @if(session('role')=="administratorius")
+                <a href="/pridetinamudarba" class="ml-10 bg-teal-900 px-4 py-2 rounded text-white hover:bg-blue-500 text-sm">Pridėti namų darbą</a>
+            @endif
+    @if(!$homeworkData->isEmpty())
+
+                <table class="table text-gray-400  space-y-6 border-2 border-gray-500">
+                    <thead class="bg-blue-600 text-white">
+                    <tr>
+                        <th class="p-5">Pavadinimas</th>
+                        <th class="p-5 text-left">Įrašymo data</th>
+                        <th class="p-5 text-left">Atlikti iki</th>
+                        <th class="p-5 text-left"></th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($homeworkData as $data)
+                        <tr class="bg-gray-200 lg:text-black">
+                            <td class="p-5 border-2 border-gray-500">{{$data -> Pavadinimas}}</td>
+                            <td class="p-5 border-2 border-gray-500">{{$data -> Data}}</td>
+                            <td class="p-5 border-2 border-gray-500">{{$data -> Atlikti_Iki}}</td>
+                            <td class="p-5 border-2 border-gray-500 hover:bg-blue-300 hover:text-white font-bold"><a href="/placiaunamudarbas?id={{$data -> id_Namu_Darbas}}">Plačiau</a></td>
+                        </tr>
+                    @endforeach
+                    @else
+                        <div class="p-10 cursor-default md:text-1xl text-xl hover:text-red-500 transition duration-200  font-bold text-red-600">
+                            Neturite namų darbų. :)
+                        </div>
+    @endif
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\NamuDarbasController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () { return view('PagrindinisLangas');});
-
+Route::get('/namudarbai', [NamuDarbasController::class, 'showHomework']);
+Route::get('/pridetinamudarba', [NamuDarbasController::class, 'showAddHomework']);
+Route::post('/pridetinamudarba', [NamuDarbasController::class, 'addHomework']);
+Route::get('/placiaunamudarbas', [NamuDarbasController::class, 'moreHomework']);
 
 
