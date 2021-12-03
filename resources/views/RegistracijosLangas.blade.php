488b7249c845998810a4ff1a79996f7eaecf2793
Martynas09 <martynas200012@gmail.com>
2021-12-03 20:56:50 +0200

    apremontuota registracija

--- resources/views/RegistracijosLangas.blade.php
+++ resources/views/RegistracijosLangas.blade.php
@@ -32,7 +32,7 @@
                             <label class="block text-gray-700 text-sm font-bold mb-2">
                                 amžius
                             </label>
-                            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="amzius" type="text">
+                            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="amzius" type="number">
                         </div>
 
                         <div class="mb-4">
@@ -53,7 +53,7 @@
                             <label class="block text-gray-700 text-sm font-bold mb-2">
                                 asmens kodas
                             </label>
-                            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="asmensKodas" type="text">
+                            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" name="asmensKodas" type="number">
                         </div>
 
                         <div class="mb-4">
