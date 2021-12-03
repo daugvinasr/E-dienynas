bdbeb00943bb1e9f6617c8bf4f4de84a35167f70
ormsher <osmis100@gmail.com>
2021-12-03 11:15:46 +0200

    sutvarkyti blade

--- resources/views/AtsiliepimoForma.blade.php
+++ resources/views/AtsiliepimoForma.blade.php
@@ -1,7 +1,10 @@
+@extends('layouts.PagrindisLangasSablonas.php')
+
+
 @section('content')
     <section class="container mx-auto p-6 rounded-10">
         <div class="w-full mb-8 overflow-hidden rounded-lg shadow-lg px-2 py-2">
-
+            <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus ut placeat sint perspiciatis corrupti assumenda nihil explicabo excepturi voluptate voluptas. Iusto, nulla ex atque odit rerum voluptate obcaecati maiores! Dignissimos est, enim aspernatur possimus unde earum corporis ab? Voluptate aliquam est dignissimos commodi doloremque nobis vel at, perspiciatis quasi molestiae?</h1>
         </div>
     </section>
 @endsection
--- resources/views/TvarkarascioFailas.blade.php
+++
@@ -1,7 +0,0 @@
-@section('content')
-    <section class="container mx-auto p-6 rounded-10">
-        <div class="w-full mb-8 overflow-hidden rounded-lg shadow-lg px-2 py-2">
-
-        </div>
-    </section>
-@endsection
