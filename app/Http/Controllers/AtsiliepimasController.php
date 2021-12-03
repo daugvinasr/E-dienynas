5f4ac4bc83c99eb407d98d2bc8027eb817d1c894
ormsher <osmis100@gmail.com>
2021-12-03 17:57:15 +0200

    Atsiliepimo Rodymas

--- app/Http/Controllers/AtsiliepimasController.php
+++ app/Http/Controllers/AtsiliepimasController.php
@@ -2,9 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Atsiliepimai;
+use App\Models\Mokiniai;
 use Illuminate\Http\Request;
 
 class AtsiliepimasController extends Controller
 {
-    //
+    public function rodytiAtsiliepimoForma($id_mokinys)
+    {
+        $atsiliepimoInformacija = Atsiliepimai::select('*')
+            ->where([
+                ['fk_Mokinys', '=', $id_mokinys],
+            ])
+            ->get();
+
+        if (!$atsiliepimoInformacija->isEmpty()) {
+            return view('AtsiliepimoForma', ['atsiliepimoInformacija' => $atsiliepimoInformacija]);
+        } else {
+            return redirect('/');
+        }
+    }
+
+
 }
--- app/Models/Mokiniai.php
+++ app/Models/Mokiniai.php
@@ -8,6 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Mokiniai extends Model
 {
     use HasFactory;
+
     protected $table = 'Mokiniai';
     public $timestamps = false;
+
+    public function mokinysToNaudotojas()
+    {
+        return $this->belongsTo(Naudotojai::class, 'fk_Naudotojas', 'id_Naudotojas');
+    }
+
 }
