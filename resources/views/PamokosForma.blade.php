e830bf62e48f45400245d28360bf5f70decd01ae
ormsher <osmis100@gmail.com>
2021-12-03 11:06:41 +0200

    visi views

---
+++ resources/views/PamokosForma.blade.php
@@ -0,0 +1,7 @@
+@section('content')
+    <section class="container mx-auto p-6 rounded-10">
+        <div class="w-full mb-8 overflow-hidden rounded-lg shadow-lg px-2 py-2">
+
+        </div>
+    </section>
+@endsection
